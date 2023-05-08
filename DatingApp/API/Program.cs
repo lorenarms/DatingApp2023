acdeebc18802e0b3a01ad1934996e4638f341f56
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-05-08 16:12:34 -0700

    connected backend API to frontend Angular

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -11,9 +11,14 @@ builder.Services.AddDbContext<DataContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
