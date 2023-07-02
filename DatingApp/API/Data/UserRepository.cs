b09c5ce9b8260583f79a67c6e63e381f92353b1e
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-07-01 17:25:13 -0700

    added action filter to update lastActive

--- DatingApp/API/Data/UserRepository.cs
+++ DatingApp/API/Data/UserRepository.cs
@@ -40,6 +40,13 @@ namespace API.Data
 
             query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
 
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(u => u.Created),
+                _ => query.OrderByDescending(u => u.LastActive)
+            };
+            
+
             
             return await PagedList<MemberDTO>.CreateAsync(
                 query.AsNoTracking().ProjectTo<MemberDTO>(_mapper.ConfigurationProvider), 
