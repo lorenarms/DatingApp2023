43b0f15346d0cac1c2fbeba46cb1dd91bbadbdc1
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-07-02 10:17:20 -0700

    optimized activity action, added id endpoint

--- DatingApp/API/Extensions/ClaimsPrincipleExtensions.cs
+++ DatingApp/API/Extensions/ClaimsPrincipleExtensions.cs
@@ -5,8 +5,15 @@ namespace API.Extensions
 {
     public static class ClaimsPrincipleExtensions
     {
-        public static string GetUsername(this ClaimsPrincipal user){
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
             return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         }
+
     }
 }
