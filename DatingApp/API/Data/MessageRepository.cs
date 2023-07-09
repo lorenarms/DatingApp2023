df5835a0175e65569b699349be8b2244a0ce7b9f
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-07-09 15:00:47 -0700

    returning the message thread

--- DatingApp/API/Data/MessageRepository.cs
+++ DatingApp/API/Data/MessageRepository.cs
@@ -4,6 +4,7 @@ using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
@@ -55,9 +56,35 @@ namespace API.Data
             
         }
 
-        public Task<IEnumerable<MessageDTO>> GetMessageThread(int CurrentUserId, int recipientId)
+        public async Task<IEnumerable<MessageDTO>> GetMessageThread(string currentUserName, string recipientUserName)
         {
-            throw new NotImplementedException();
+            var messages = await _context.Messages
+                .Include(u => u.Sender).ThenInclude(p => p.Photos)
+                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+                .Where(
+                    m => m.RecipientUsername == currentUserName && 
+                    m.SenderUsername == recipientUserName ||
+                    m.RecipientUsername == recipientUserName &&
+                    m.SenderUsername == currentUserName
+                )
+                .OrderByDescending(m => m.MessageSent)
+                .ToListAsync();
+
+            var unreadMessages = messages.Where(m => m.DateRead == null && 
+                m.RecipientUsername == currentUserName).ToList();
+
+            if (unreadMessages.Any())
+            {
+                foreach (var message in unreadMessages)
+                {
+                    message.DateRead = DateTime.UtcNow;
+                }
+
+                await _context.SaveChangesAsync();
+
+            }
+
+            return _mapper.Map<IEnumerable<MessageDTO>>(messages);
         }
 
         public async Task<bool> SaveAllAsync()
--- DatingApp/API/Interfaces/IMessageRepository.cs
+++ DatingApp/API/Interfaces/IMessageRepository.cs
@@ -12,7 +12,7 @@ namespace API.Interfaces
         void DeleteMessage(Message message);
         Task<Message> GetMessage(int id);
         Task<PagedList<MessageDTO>> GetMessagesForUser(MessageParams messageParams);
-        Task<IEnumerable<MessageDTO>> GetMessageThread(int CurrentUserId, int recipientId);
+        Task<IEnumerable<MessageDTO>> GetMessageThread(string currentUserName, string recipientUserName);
         Task<bool> SaveAllAsync();
 
     }
