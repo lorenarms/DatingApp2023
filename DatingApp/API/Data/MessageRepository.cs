417cd2a68ccb18b4fe387b45157bf1ec1122457a
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-07-12 15:26:17 -0700

    added delete message to api

--- DatingApp/API/Data/MessageRepository.cs
+++ DatingApp/API/Data/MessageRepository.cs
@@ -42,9 +42,13 @@ namespace API.Data
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username),
-                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username),
-                _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.DateRead == null)
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username
+                    && u.RecipientDeleted == false),
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username
+                    && u.SenderDeleted == false),
+                _ => query.Where(u => u.RecipientUsername == messageParams.Username 
+                    && u.RecipientDeleted == false 
+                    && u.DateRead == null)
             };
 
             var messages = query.ProjectTo<MessageDTO>(_mapper.ConfigurationProvider);
@@ -63,8 +67,11 @@ namespace API.Data
                 .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(
                     m => m.RecipientUsername == currentUserName && 
-                    m.SenderUsername == recipientUserName ||
-                    m.RecipientUsername == recipientUserName &&
+                    m.RecipientDeleted == false &&
+                    m.SenderUsername == recipientUserName 
+                    ||
+                    m.RecipientUsername == recipientUserName && 
+                    m.SenderDeleted == false &&
                     m.SenderUsername == currentUserName
                 )
                 .OrderBy(m => m.MessageSent)
