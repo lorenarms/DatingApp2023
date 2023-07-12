9e1879caccc4568743384ca4332bd816873a5ce2
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-07-12 15:43:13 -0700

    added message deleting to client

--- DatingApp/client/src/app/messages/messages.component.ts
+++ DatingApp/client/src/app/messages/messages.component.ts
@@ -34,11 +34,20 @@ export class MessagesComponent implements OnInit {
     })
   }
 
+
+  deleteMessage(id: number) {
+    this.messageService.deleteMessage(id).subscribe({
+      next: _ => this.messages?.splice(this.messages.findIndex(m => m.id == id), 1)
+    })
+  }
+
   pageChanged(event: any) {
     if (this.pageNumber !== event.page) {
       this.pageNumber = event.page;
       this.loadMessages();
     }
   }
+
+
 
 }
