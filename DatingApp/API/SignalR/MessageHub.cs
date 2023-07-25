7cd00adb676212d7f4c26064df5d89776ee3883b
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-07-25 15:08:40 -0700

    fixed messages retrieving and sending

--- DatingApp/API/SignalR/MessageHub.cs
+++ DatingApp/API/SignalR/MessageHub.cs
@@ -29,7 +29,8 @@ namespace API.SignalR
             var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
-            var messages = await _messageRepository.GetMessageThread(Context.User.GetUsername(), otherUser);
+            var messages = await _messageRepository
+                .GetMessageThread(Context.User.GetUsername(), otherUser);
 
             await Clients.Group(groupName).SendAsync("RecieveMessageThread", messages);
 
